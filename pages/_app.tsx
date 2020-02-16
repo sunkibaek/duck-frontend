aced8f8bad09001b7a0a58750ce1568d5622888d
Sunki Baek <sunki.baek@gmail.com>
2020-02-16 09:13:03 -0800

    Add background color to body

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../css/main.css";
 
 const App = ({ Component, pageProps }: any) => {
   return <Component {...pageProps} />;
