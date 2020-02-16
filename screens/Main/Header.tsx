8214c9864cd98c6b813baab0b239df26bdba77c5
Sunki Baek <sunki.baek@gmail.com>
2020-02-16 09:36:37 -0800

    Restructure files to their purposes

---
+++ screens/Main/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => (
+  <div className="row mb-5">
+    <div className="col">
+      <h2 className="text-center">Duck Feeding Log System</h2>
+    </div>
+  </div>
+);
+
+export default Header;
---
+++ screens/Main/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import Body from "./Body";
+
+const MainContainer = () => (
+  <div className="container py-5">
+    <Header />
+
+    <Body />
+  </div>
+);
+
+export default MainContainer;
