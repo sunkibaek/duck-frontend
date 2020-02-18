a03dfb60a6f2897d12c7cc6f26dd1c1fa242961e
Sunki Baek <sunki.baek@gmail.com>
2020-02-18 02:27:54 -0800

    Edit to show alert when insertion succeeded

--- screens/Main/index.tsx
+++ screens/Main/index.tsx
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Body from "./Body";
 import GraphQL from "../../services/GraphQL";
 import { FoodCategory } from "types";
 
+const ALERT_TIMEOUT = 3000;
+
 const MainContainer = () => {
   const [isFormSubmitting, setIsFormSubmitting] = useState(false);
+  const [isAlertVisible, setIsAlertVisible] = useState(false);
+
+  useEffect(() => {
+    if (isAlertVisible) {
+      setTimeout(() => {
+        setIsAlertVisible(false);
+      }, ALERT_TIMEOUT);
+    }
+  }, [isAlertVisible]);
 
   return (
     <div className="container py-5">
@@ -15,31 +26,30 @@ const MainContainer = () => {
         onFormSubmit={async formInputs => {
           setIsFormSubmitting(true);
 
-          try {
-            await GraphQL.insertFeedingLog({
-              dateTime: new Date(
-                `${formInputs.date} ${formInputs.time}`
-              ).toISOString(),
-              location: formInputs.location,
-              howMany: Number(formInputs.howMany),
-              foodCategory: formInputs.foodCategory as FoodCategory,
-              food: formInputs.foodSubcategory,
-              foodQuantity: Number(formInputs.foodQuantity)
-            });
-          } catch (e) {
-            console.log(e.message);
-          } finally {
-            setIsFormSubmitting(false);
-          }
+          await GraphQL.insertFeedingLog({
+            dateTime: new Date(
+              `${formInputs.date} ${formInputs.time}`
+            ).toISOString(),
+            location: formInputs.location,
+            howMany: Number(formInputs.howMany),
+            foodCategory: formInputs.foodCategory as FoodCategory,
+            food: formInputs.foodSubcategory,
+            foodQuantity: Number(formInputs.foodQuantity)
+          });
+
+          setIsFormSubmitting(false);
+          setIsAlertVisible(true);
         }}
         isFormSubmitting={isFormSubmitting}
       />
 
-      <div className="container fixed-top pt-3">
-        <div className="alert alert-primary" role="alert">
-          Submitted successfully!
+      {isAlertVisible ? (
+        <div className="container fixed-top pt-3">
+          <div className="alert alert-primary" role="alert">
+            Submitted successfully!
+          </div>
         </div>
-      </div>
+      ) : null}
     </div>
   );
 };
